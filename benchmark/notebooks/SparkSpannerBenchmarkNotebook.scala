40b76fe933f28f77fb8f33da16c2247662b1ae68
Max Ksyunz <max.ksyunz@improving.com>
2026-02-10 11:52:01 -0800

    Fix size estimate calculation
    Signed-off-by: Max Ksyunz <max.ksyunz@improving.com>

--- benchmark/notebooks/SparkSpannerBenchmarkNotebook.scala
+++ benchmark/notebooks/SparkSpannerBenchmarkNotebook.scala
@@ -78,7 +78,7 @@ val dfWrite = dfSource.limit(numRecords.toInt).cache()
 
 val averageRowSizeBytes = SizeEstimator.estimate(dfWrite.first())
 val sizeInBytes = averageRowSizeBytes * numRecords
-val sizeMb = sizeInBytes / (1024 * 1024)
+val sizeMb = sizeInBytes / (1024.0 * 1024.0)
 println(s"Estimated job size: $sizeMb MB")
 println(f"Average row size: $averageRowSizeBytes bytes")
 println(s"Number of partitions: $numPartitions")
